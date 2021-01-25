e28a1043767c9ca613c8106d54dab3a8f9da8d88
champiao <rafael.leite.assis63@gmail.com>
2021-01-25 20:31:41 -0300

    second commit

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt update -y; sudo apt upgrade -y
+sudo apt install nginx -y 
+sudo apt install postfix -y 
+sudo apt install dovecot -y 
+sudo apt remove --purge php5* -y 
+sudo add-apt-repository ppa:ondrej/php -y 
+sudo apt update -y 
+sudo apt install php5.6-common php5.6-cli php5.6 php5.6-mysql -y 
+wget http://repo.mysql.com/mysql-apt-config_0.8.9-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.9-1_all.deb
+sudo apt update -y 
+sudo apt install mysql-server -y 
+wget https://github.com/roundcube/roundcubemail/releases/download/1.3.8/roundcubemail-1.3.8.tar.gz
+tar -xvf roundcubemail-1.3.8.tar.gz
+sudo mv roundcubemail-1.3.8 /usr/share/nginx/roundcubemail
+sudo mv champiaoFile /tmp
+cd /usr/share/nginx/roundcubemail
+sudo apt install composer
+rm -rf composer.json-dist
+mv /tmp/champiaoFile ./composer.json
+sudo apt install php-net-ldap2 php-net-ldap3
+sudo composer install --no-dev
+sudo apt install unzip
+sudo ./bin/install-jsdeps.sh
+sudo chown www-data:www-data temp/ logs/ -R
+
+

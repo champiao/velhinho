82f920b50fa9dbc183d36abfe62f10d9389f66bf
champiao <rafael.leite.assis63@gmail.com>
2021-01-25 21:18:46 -0300

    i

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y; sudo apt upgrade -y -f
-sudo apt install openssh-client openssh-server -y
+sudo apt install openssh-client openssh-server -y -f
 sudo apt install nginx -y 
 sudo apt install postfix -y 
 sudo apt install dovecot -y 
